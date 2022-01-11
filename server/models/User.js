f0da81033f968638a27ea55ccea9d5a1bbe00ada
AQB <abrown0926>
2022-01-11 13:34:55 -0600

    updated user model added routes and controllers

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const { Schema } = mongoose;
 const Post = require("./Post");
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required",
+  },
   username: {
     type: String,
     unique: true,
@@ -11,15 +17,34 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
+    trim: true,
     unique: true,
     match: [/.+@.+\..+/, "Must match an email address!"],
+    required: "Email is required",
   },
-  password: {
+  hashed_password: {
     type: String,
-    minlength: 8,
+    required: "Password is required",
+  },
+  salt: String,
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now,
   },
   createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  hashed_password: {
     type: String,
+    required: "Password is required",
   },
   posts: [
     {
@@ -28,6 +53,46 @@ const userSchema = new Schema({
     },
   ],
 });
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.path("hashed_password").validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate("password", "Password must be at least 6 characters.");
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate("password", "Password is required");
+  }
+}, null);
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
 
 const User = mongoose.model("User", userSchema);
 
