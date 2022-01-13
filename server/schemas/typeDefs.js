4807b331c2f03a05aa1954e2457ee38c2cf49c07
AQB <abrown0926>
2022-01-12 21:19:34 -0600

    react and server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,39 +2,31 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   # Define which fields are accessible from the Class model
-  type Class {
-    _id: ID
-    name: String
-    building: String
-    creditHours: Int
+  type Post {
+    _id: ID!
+    text: String!
+    createdAt: String!
+    name: String!
+    email: String!
   }
   type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    posts: [Post]!
-  }
-  type Post {
-    _id: ID
-    postText: String
-    postAuthor: String
-    createdAt: String
-    comments: [Comment]!
-  }
-  type Comment {
-    _id: ID
-    commentText: String
-    createdAt: String
+    _id: ID!
+    name: String!
+    email: String!
+    hashed_password: String!
+    about: String
+    createdAt: String!
   }
 
   # Define which queries the front end is allowed to make and what data is returned
   type Query {
-    classes: [Class]
-    users: [User]
-    user(username: String!): User
-    posts(username: String): [Post]
+    getPosts: [Post]
+    name: String
+    email: [User]
+    user(email: String!): User
+    posts(name: String): [Post]
     post(postId: ID!): Post
+    comments(name: String): User
     me: User
   }
 `;
