e6ab55ad659f898fd02a036f161a8106fe26d102
AQB <abrown0926>
2022-01-12 19:17:38 -0600

    updated client and server folders

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,27 +1,58 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const PostSchema = new mongoose.Schema({
-  text: {
+const User = require("./User");
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  postText: {
     type: String,
-    required: "Text is required",
+    minlength: 1,
+    maxlength: 1000,
+    trim: true,
   },
-  photo: {
-    data: Buffer,
-    contentType: String,
+  postAuthor: {
+    type: String,
+    required: true,
+    trim: true,
   },
-  likes: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
-  comments: [
-    {
-      text: String,
-      created: { type: Date, default: Date.now },
-      postedBy: { type: mongoose.Schema.ObjectId, ref: "User" },
-    },
-  ],
-  postedBy: { type: mongoose.Schema.ObjectId, ref: "User" },
-  created: {
+  createdAt: {
     type: Date,
     default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
   },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 1000,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+  likes: [
+    {
+      username: {
+        type: String,
+        createdAt: String,
+      },
+    },
+  ],
+  users: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
 });
 
-export default mongoose.model("Post", PostSchema);
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
---
+++ server/seeders/seed.js
@@ -0,0 +1,64 @@
+const db = require("../config/connection");
+const { School, Class, Professor, Post, User } = require("../models");
+
+const schoolData = require("./schoolData.json");
+const classData = require("./classData.json");
+const professorData = require("./professorData.json");
+const postData = require("./postData.json");
+const userSeeds = require("./userSeeds.json");
+
+db.once("open", async () => {
+  try {
+    // clean database
+    await School.deleteMany({});
+    await Class.deleteMany({});
+    await Professor.deleteMany({});
+    await Post.deleteMany({});
+    await User.deleteMany({});
+    await Post.create(postData);
+    await User.create(userSeeds);
+
+    // bulk create each model
+    const schools = await School.insertMany(schoolData);
+    const classes = await Class.insertMany(classData);
+    const professors = await Professor.insertMany(professorData);
+    // const posts = await Post.insertMany(postData);
+    // const users = await User.insertMany(userSeeds);
+
+    for (newClass of classes) {
+      // randomly add each class to a school
+      const tempSchool = schools[Math.floor(Math.random() * schools.length)];
+      tempSchool.classes.push(newClass._id);
+      await tempSchool.save();
+
+      // randomly add a professor to each class
+      const tempProfessor =
+        professors[Math.floor(Math.random() * professors.length)];
+      newClass.professor = tempProfessor._id;
+      await newClass.save();
+
+      // reference class on professor model, too
+      tempProfessor.classes.push(newClass._id);
+      await tempProfessor.save();
+
+      for (let i = 0; i < postData.length; i++) {
+        const { _id, postAuthor } = await Post.create(postData[i]);
+        const user = await User.findOneAndUpdate(
+          { username: postAuthor },
+          {
+            $addToSet: {
+              posts: _id,
+            },
+          }
+        );
+      }
+    }
+
+     console.log("all done!");
+     process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  
+  }
+});
