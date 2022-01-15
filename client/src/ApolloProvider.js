76e32c5c8eb5f073c9ee760eb419c28f1902e8b0
AQB <abrown0926>
2022-01-15 16:21:06 -0600

    post, comment, delete, and add

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import App from "./App";
+// import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+// import { createHttpLink } from "apollo-link-http";
+
+// const httplink = createHttpLink({
+//   uri: "http://localhost:3000",
+// });
+
+// const client = new ApolloClient({
+//   link: httplink,
+//   cache: new InMemoryCache(),
+// });
+
+// export default (
+//   <ApolloProvider client={client}>
+//     <App />
+//   </ApolloProvider>
+// );
