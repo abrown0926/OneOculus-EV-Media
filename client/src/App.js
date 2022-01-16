d3003f4bcfbfa88a03b14704eb86c93527118541
JoeyDela <duffy.west87@gmail.com>
2022-01-15 19:41:41 -0600

    username and pw placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ const FormExampleInverted = () => (
   <Segment inverted>
     <Form inverted>
       <Form.Group widths="equal">
-        <Form.Input fluid label="First name" placeholder="First name" />
-        <Form.Input fluid label="Last name" placeholder="Last name" />
+        <Form.Input fluid label="User Name" placeholder="User Name" />
+        <Form.Input fluid label="Password" placeholder="Password" />
       </Form.Group>
       <Form.Checkbox label="I agree to the Terms and Conditions" />
-      <Button type="submit">Submit</Button>
+      <Button type="submit">Login</Button>
     </Form>
   </Segment>
 );
