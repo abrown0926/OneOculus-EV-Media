f0b01eb5330d15e536a4ef152f375e6ae25522a6
JoeyDela <duffy.west87@gmail.com>
2022-01-15 19:12:08 -0600

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import Home from "./pages/home/Home";
-// import { Button } from "semantic-ui-react";
-import MenuBar from "./components/navbar/NavBar";
+import React from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
 
-function App() {
-  return <Home />;
-}
-
-export default App;
+const FormExampleInverted = () => (
+  <Segment inverted>
+    <Form inverted>
+      <Form.Group widths="equal">
+        <Form.Input fluid label="First name" placeholder="First name" />
+        <Form.Input fluid label="Last name" placeholder="Last name" />
+      </Form.Group>
+      <Form.Checkbox label="I agree to the Terms and Conditions" />
+      <Button type="submit">Submit</Button>
+    </Form>
+  </Segment>
+);
+export default FormExampleInverted;
